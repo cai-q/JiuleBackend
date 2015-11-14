831d5cf176906c6766a8f7251f9c622d30906a5d
蔡乾 <cai.q@foxmail.com>
2015-11-14 19:31:10 +0800

    add member test.

---
+++ app/Member.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Member
+ * @package App
+ * @mixin \Eloquent
+ */
+class Member extends Model
+{
+    protected $connection = 'mysql_old';
+
+    protected $table = 'member';
+}
